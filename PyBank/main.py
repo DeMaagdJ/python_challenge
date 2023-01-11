3ac3d973ce3312e8a0ac6a4aef0748c45dfb61a3
Justin DeMaagd <Justin@iMac.local>
2023-01-10 19:00:47 -0800

    Modifications to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,7 @@
+
 import os
 import csv
 
-def min_():
-    return min(zip(dictionary.keys(), dictionary.values()))
-
-def max_():
-    return max(zip(dictionary.keys(), dictionary.values()))
-   
 #Path to collect data from the resources folder
 budget_data_csv = os.path.join("/Users/Justin/Documents/Bootcamp_Home folder/homework/Module 3/python_challenge/PyBank/Resources/budget_data.csv")
 
@@ -14,41 +9,61 @@ print(f'                   ')
 print(f'Financial Analysis')
 print(f'---------------------------')
 
-
-
-sum_1 = 1
-total_months = 1
-list_1 = []
+sum_1 = 0
+total_months = 0
+changeList = []
 dictionary= {}
+
+start_val_set = False
+last_val = 0
 
 #open and read csv
 with open(budget_data_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     csv_header = next(csv_reader)
-    
-    #Work around to reset reader to restart iteration at line after header
-    csv_reader_row1 = next(csv_reader)
-    csv_first_row = int(csv_reader_row1[1])
-    sum_1 += csv_first_row
+
 
     for row in csv_reader:
         
+        date_id = row[0]
+        value_id = int(row[1]) #Iterated values in profit/losses
+        
+        
+        
+        if start_val_set == False:
+            last_val = value_id
+            start_val_set = True
+              
+           
+         
         total_months += 1 #Iterated total months
         value_id = int(row[1]) #Iterated values in profit/losses
         sum_1 = sum_1 + int(value_id) #Formula for Total Amount
-        change = value_id - csv_first_row #Formula to calculate change in values and have it iterated
-        csv_first_row = value_id #Restart iterating values in profit/losses to populate into list
-        list_1.append(change) #Create list to determine total number of months as variable to find the average
-        dictionary[row[0]] = row[1]
-        
+        change = value_id - last_val #Formula to calculate change in values and have it iterated
+        last_val = value_id #Restart iterating values in profit/losses to populate into list
+        dictionary[date_id] = change
+
+minval = min(dictionary.values())
+date_of_min = [k for k, v in dictionary.items() if v==minval]
+maxval = max(dictionary.values())
+date_of_max = [k for k, v in dictionary.items() if v==maxval]
+
+date_of_min[0]
+date_of_max[0]
+
+total_months = len(dictionary.keys())
+
+s = 0
+for i in dictionary.values():
+    s = s + i
+    
+s
+
 print("Total Months: " + str(total_months))
 print("Total: $" + str(sum_1))
-print("Average Change: $" + str(sum(list_1)/len(list_1)))
-print("Greatest Increase in Profits: ", min_())
-print("Greatest Decrease in Profits: ", max_())
+print("Average Change: $" + str(s/(total_months-1)))
+print("Greatest Increase in Profits: ", date_of_max[0], (maxval))
+print("Greatest Decrease in Profits: ", date_of_min[0], (minval))
 
 
-
-   
-       
 
